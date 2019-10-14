53eb8488f30adb521c6eb541d87c179701dd1434
League Student <league@iMac-16.attlocal.net>
2019-10-13 17:25:57 -0700

    I did League Invaders (IP)

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -5,33 +5,52 @@ import java.util.Random;
 public class ObjectManager {
 	
 	Rocketship rocket;
-	Projectile projectiles;
-	Alien aliens;
+	ArrayList<Projectile> projectiles= new ArrayList<Projectile>();
+	ArrayList<Alien> aliens= new ArrayList<Alien>();
 	Random random= new Random();
 	
-	ObjectManager(Rocketship r, ArrayList p, ArrayList a){
+	ObjectManager(Rocketship r){
 		this.rocket=r;
-		//p.add(projectiles);
-		//a.add(aliens);
 	}
 	
 	void addProjectile(Projectile p) {
-		
+		projectiles.add(p);
+		for(Projectile j : projectiles) {
+			j.update();
+			if(j.y<=0) {
+				j.isActive = false;
+			}
+		}
 	}
 	
 	void addAlien(){
-		//aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH),0,50,50));
+		aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH),0,50,50));
 	}
 	
 	void update() {
-		
+		for(Alien i : aliens) {
+			i.update();
+			if(i.y>=LeagueInvaders.HEIGHT) {
+				i.isActive =false;
+			}
+		}
 	}
 	
-	void draw(Graphics graphics){
-		
+	void draw(Graphics g){
+		rocket.draw(g);
+		for(Alien a : aliens) {
+			a.draw(g);
+		}
+		for(Projectile p : projectiles) {
+			p.draw(g);
+		}
 	}
 	
 	void purgeObjects(){
-		
+		for(int i=0; i<aliens.size(); i++) {
+			if(aliens.get(i).isActive ==false) {
+				aliens.remove(i);
+			}
+		}
 	}
 }
